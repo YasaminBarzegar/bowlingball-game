6f15446e68bdc470904cd1108947595c87a50fac
YasaminBarzegar <yasaminbarzegar2024@gmail.com>
2025-01-21 15:40:39 +0330

    fix ball & camera

--- Assets/Scripts/Bowl.cs
+++ Assets/Scripts/Bowl.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Bowl : MonoBehaviour
+public class BallMoveController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+     private Vector3 startPosition;   
+    private Vector3 endPosition;     
+    private bool isDragging = false; 
+
+    
+
+    public float forceMultiplier = 10f; 
+
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            startPosition = Input.mousePosition; 
+            isDragging = true;
+        }
+
+       
+        if (Input.GetMouseButtonUp(0) && isDragging)
+        {
+            endPosition = Input.mousePosition; 
+            LaunchBall(); 
+            isDragging = false; 
+        }
+    }
+
+    void LaunchBall()
+    {
+        Vector3 direction = (startPosition - endPosition).normalized; 
+        float distance = Vector3.Distance(startPosition, endPosition); 
+
+        rb.useGravity = true; 
+        rb.AddForce(new Vector3(direction.x, 0, direction.y) * distance * forceMultiplier); 
     }
 }
